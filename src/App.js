b0fd39e9172dba3c555546be827f97c1060eda4b
Frantisek Brothanek <franta.brothanek@gmail.com>
2023-05-26 23:20:14 +0200

    Boilerplate, eslint, .env, gitignore

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import Mainframe from './containers/Mainframe'
 
 function App() {
+  console.log('process.env.REACT_APP_COLUMNS', process.env.REACT_APP_COLUMNS)
+  console.log('process.env.REACT_APP_ROWS', process.env.REACT_APP_ROWS)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Mainframe />
   );
 }
 
---
+++ src/containers/Mainframe.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Mainframe = () => {
+  return (
+    <div>Mainframe</div>
+  );
+}
+
+export default Mainframe;
